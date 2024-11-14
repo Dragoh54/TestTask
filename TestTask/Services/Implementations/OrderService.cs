01a69bddb7a1688d6812a95a2ba2771cb8f990a7
Dragoh54 <ndragoh@gmail.com>
2024-11-15 01:17:06 +0300

    feat: add context and constructors to services

--- TestTask/Services/Implementations/OrderService.cs
+++ TestTask/Services/Implementations/OrderService.cs
@@ -1,10 +1,18 @@
-﻿using TestTask.Models;
+﻿using TestTask.Data;
+using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations;
 
 public class OrderService : IOrderService
 {
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrderService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
     public Task<Order> GetOrder()
     {
         throw new NotImplementedException();
--- TestTask/Services/Implementations/UserService.cs
+++ TestTask/Services/Implementations/UserService.cs
@@ -1,10 +1,18 @@
-﻿using TestTask.Models;
+﻿using TestTask.Data;
+using TestTask.Models;
 using TestTask.Services.Interfaces;
 
 namespace TestTask.Services.Implementations;
 
 public class UserService : IUserService
 {
+    private readonly ApplicationDbContext _dbContext;
+
+    public UserService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
     public Task<User> GetUser()
     {
         throw new NotImplementedException();
